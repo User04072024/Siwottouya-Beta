81a5c78d7aa066efbf98092f024cd613af305b6e
User04072024 <alfredojoseamaya4580@gmail.com>
2025-11-12 23:12:12 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ Ajusta el nombre de tu repositorio aquí:
 export default defineConfig({
   plugins: [react()],
-  base: '/Siwottouya-Beta/'
+  base: '/Siwottouya-Beta/',
 })
+
+
 
 
