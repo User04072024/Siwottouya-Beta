0ea9102a4f3d77ee1928d516007e90a7ec85f50c
User04072024 <alfredojoseamaya4580@gmail.com>
2025-11-12 23:09:29 -0500

    Update Vite config to include base path
    Set base path for deployment in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
+// ⚠️ Ajusta el nombre de tu repositorio aquí:
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/Siwottouya-Beta/'
 })
+
 
