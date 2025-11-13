edf9f003998b1867f9a8dcbb0e84bece6dd1882b
User04072024 <alfredojoseamaya4580@gmail.com>
2025-11-12 23:14:39 -0500

    Enhance Vite config with path aliasing
    Added path alias for easier imports.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  // SOLUCIÓN: Añadir la ruta base de tu repositorio
+  base: '/Siwottouya-Beta/', 
+  
   plugins: [react()],
-  base: '/Siwottouya-Beta/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
-
 
 
 
